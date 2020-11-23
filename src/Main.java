0f8f39cc36f40392c64358d315613655e371593c
Christos Pantazopoulos <pantazopc@gmail.com>
2020-11-23 21:07:50 +0200

    23/11

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner name = new Scanner(System.in);
+
         System.out.println("Hello, let's play Buzz!");
+        System.out.println("Press enter to continue...");try{        System.in.read();}catch(Exception e){	e.printStackTrace();}
+        System.out.println("Enter Username");
+
+        String userName = name.nextLine();
+
+        System.out.println("Hello " + userName + ", are you ready to play? \nPress Enter to start playing Buzz or press Esc to exit."); try{        System.in.read();}catch(Exception e){	e.printStackTrace();}
+
     }
 }
